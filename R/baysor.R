55ecf9ecf96b80eae18348e043d1346c9ab464ed
Ilya Korsunsky <ilya.korsunsky@gmail.com>
2022-12-06 03:17:36 +0000

    baysor

--- R/baysor.R
+++ R/baysor.R
@@ -215,7 +215,7 @@ baysor.run <- function(
     message('WARNING: for now, tx needs to have columns: x, y, gene, cell')
     stopifnot(colnames(tx) == c('x', 'y', 'gene', 'cell'))
     stopifnot(file.exists(fname_baysor_config))
-    if (baysor_mode == 'docker') {
+    if (opts$baysor_mode == 'docker') {
         if (!'docker' %in% strsplit(system('groups', intern = TRUE), ' ')[[1]]) {
             stop('You do not belong to the docker group. Please follow the instructions here to run docker without root privileges: https://docs.docker.com/engine/install/linux-postinstall/')
         }
@@ -223,9 +223,9 @@ baysor.run <- function(
     
     ntiles <- baysor.split_tx_files(output_dir, opts$max_tx_per_grid) 
     baysor.prepare_cmds(ntiles, output_dir, fname_baysor_config, opts$max_parallel_jobs)
-    if (baysor_mode == 'docker') {
+    if (opts$baysor_mode == 'docker') {
         baysor.run_baysor_tiles_docker(ntiles, output_dir, fname_baysor_config)    
-    } else if (baysor_mode == 'binary') {
+    } else if (opts$baysor_mode == 'binary') {
         stop('binary mode not implemented yet. Please use docker mode.')
         # baysor.run_baysor_tiles_binary(ntiles, output_dir, ..., fname_baysor_config) 
     } else {
